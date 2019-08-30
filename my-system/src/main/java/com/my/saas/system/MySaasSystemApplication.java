d71d8ae294db37e5c3150e5d0c877205e9bcab96
ni.hua <ni.hua@cesgroup.com>
2019-08-30 17:14:06 +0800

    测试类名称调整

--- my-system/src/main/java/com/my/saas/system/MySaasSystemApplication.java
+++ my-system/src/main/java/com/my/saas/system/MySaasSystemApplication.java
@@ -1,13 +1,17 @@
 package com.my.saas.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.my.saas.system", "com.my.saas.common"})
+@MapperScan(basePackages = "com.my.saas.**.dao")
 public class MySaasSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySaasSystemApplication.class, args);
+        SpringApplication.run(MySaasSystemApplication.class);
     }
 
 }
